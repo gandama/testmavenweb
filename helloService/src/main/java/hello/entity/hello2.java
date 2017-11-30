0fef185761257f5635d520d77a1add30c96c8a6e
gandama <yangwenduo110@163.com>
2017-11-30 17:27:19 +0800

    新增 hello2接口以及其实现类

---
+++ helloService/src/main/java/hello/entity/hello2.java
@@ -0,0 +1,8 @@
+package hello.entity;
+
+/**
+ * Created by yang on 2017/11/30.
+ */
+public interface hello2 {
+    public String sayhello();
+}
---
+++ helloService/src/main/java/hello/entity/impl/hello2Impl.java
@@ -0,0 +1,12 @@
+package hello.entity.impl;
+
+import hello.entity.hello2;
+
+/**
+ * Created by yang on 2017/11/30.
+ */
+public class hello2Impl implements hello2 {
+    public String sayhello() {
+        return "hello";
+    }
+}
